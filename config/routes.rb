655d9a55a9084fb3588b7490e8605ec225ec86f4
kazukihori <git123.hoppy@gmail.com>
2024-04-02 18:23:00 +0900

    エラー確認の為、コミット

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
-  get 'uesrs/show'
+  root to: "home#index"
+  devise_for :users, controllers: {
+    registrations:  'users/registrations',
+    sessions:       'users/sessions'
+  }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
